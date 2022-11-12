f1666c4beb108437f696e9cd1562dc678451404c
Pavel_Chernobay <pavell.ua@gmail.com>
2022-11-12 22:41:04 +0200

    add new change in index.js Films info

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-const numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastFilmWatched = prompt('Какой последний фильм посмотрели?');
+let lastFilmRate = +prompt('Оцените его');
+
+personalMovieDB.movies[lastFilmWatched] = lastFilmRate;
+personalMovieDB.movies[lastFilmWatched] = lastFilmRate;
+
+lastFilmWatched = prompt('Какой последний фильм посмотрели?');
+lastFilmRate = +prompt('Оцените его');
+
+personalMovieDB.movies[lastFilmWatched] = lastFilmRate;
+personalMovieDB.movies[lastFilmWatched] = lastFilmRate;
+
+console.log(personalMovieDB);
